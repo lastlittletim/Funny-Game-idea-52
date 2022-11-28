0f346cbee7825f3e682d5d9e5a1a0c2039c8efd9
Iz <zizaaclee@gmail.com>
2022-11-28 21:24:36 +0800

    Player Controls

---
+++ Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    public Rigidbody2D rb; 
+    public float movementSpeed;
+
+    public bool canJump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get input
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        //move
+
+
+        rb.velocity = new Vector2(horizontalInput * movementSpeed  ,  rb.velocity.y);  
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            if (canJump)
+            rb.velocity = Vector2.up * movementSpeed; 
+            canJump = false;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.CompareTag("Floor"))
+        {
+            canJump = true;
+        }
+    }
+}
