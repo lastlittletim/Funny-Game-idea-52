002420034bc693dc8e8a3e776925fde54bb23d72
lastlittletim <puffstroom@gmail.com>
2022-11-28 19:36:15 +0800

    Initial Commit

---
+++ Assets/Scripts/Library.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Library : MonoBehaviour
+{
+    //this script is used exclusively to store classes, enums and other stuff that is used by multiple scripts
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //declare variables
+    public float movementSpeed;
+
+    Rigidbody2D rb;
+    public BoxCollider2D floorDetector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); //get rigidbody script
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+
+    void HandleInput()
+    {
+        rb.velocity = new Vector2( //set velocity of object
+            Input.GetAxis("Horizontal") * movementSpeed, //set horizontal velocity (x) to horizontal input (i.e. move in the direction)
+            rb.velocity.y //leave vertical velocity as is, or gravity will not work
+            );
+    }
+}
