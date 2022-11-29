3a375578b9fab9f14ba53022e266bf9d06e4f36c
lastlittletim <puffstroom@gmail.com>
2022-11-29 22:00:29 +0800

    Bullet Enemy
    kind of scuffed

--- Assets/Scripts/TestEnemy.cs
+++ Assets/Scripts/TestEnemy.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class TestEnemy : MonoBehaviour
 {
+    public GameObject bullet;
+    public float bulletInterval = 1;
+    public float timer;
+
+    //shoot
+        //make bullet
+
+    //find player
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,15 @@ public class TestEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime; //update timer
+        if(timer > bulletInterval)
+        {
+            //shoot
+            GameObject newBullet = Instantiate(bullet);
+            newBullet.transform.position = this.transform.position;
+            Rigidbody2D bulletRB = newBullet.GetComponent<Rigidbody2D>(); //get bullet rb
+            bulletRB.velocity = Vector2.right; //set velocity
+            timer = 0;
+        }
     }
 }
